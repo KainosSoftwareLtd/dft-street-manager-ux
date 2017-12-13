b1758c30bae13743a3b7b4ddeef5395149e8cc0a
Michael Kemp <michaelk@kainos.com>
2017-12-13 13:23:17 +0000

    Added sign-in page and POST routing. Updated button on start-page to link to sign-in, Added blank page for promoter-planner

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,18 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.post('/alpha/v1-0/sign-in.html', function (req, res) {
+  switch (req.body.email) {
+    case 'promo@sm.com' :
+      res.redirect('/alpha/v1-0/promoter-planner-home.html')
+      break
+    case 'auth@sm.com' :
+      res.render()
+      break
+    default:
+      res.render('alpha/v1-0/sign-in.html', {validationError: 'Invalid login credentials.'})
+  }
+})
 // Add your routes here - above the module.exports line
 
 module.exports = router
