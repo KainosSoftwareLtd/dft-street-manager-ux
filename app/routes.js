8a12563271e80f3cc1320dc1cdeb2dc1b7b00ea4
Ronan O'Connor <r.oconnor@kainos.com>
2017-12-22 15:50:08 +0000

    prepopulating edit form

--- app/routes.js
+++ app/routes.js
@@ -22,6 +22,27 @@ router.post('/alpha/v1-0/sign-in.html', function (req, res) {
 router.post('/alpha/v2-0/create-new-work.html', function (req, res) {
   res.redirect('/alpha/v2-0/success.html')
 })
+
+router.get('/alpha/v2-0/edit-work-record', function (req, res) {
+  var data = {'data' :{
+      'work-reference-number' : 'CT0224443466',
+      'promoter-name' : 'JOHN CARRINGTON',
+      'promoter-agent' : 'FM CONWAY LTD',
+      'highway-authority' : 'Highway Authority',
+      'usrn' : '08400189',
+      'description-of-work' : 'Digging up the road' }
+    }
+  res.render('alpha/v2-0/edit-work-record.html', data)
+})
+
+router.post('/alpha/v2-0/edit-work-record.html', function (req, res) {
+  res.redirect('/alpha/v2-0/edit-success.html')
+})
+
+router.get('/hello-world', function (req, res) {
+  res.render('hello-world.html', {'message' : 'Hello world'});
+});
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
