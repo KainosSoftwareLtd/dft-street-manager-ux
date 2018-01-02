2a1dfe93ac3df7ac816947200b3f8fb019043cf5
Ronan O'Connor <r.oconnor@kainos.com>
2018-01-02 14:33:58 +0000

    Adding validation to edit-work-record

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+var data = {'data' :{
+  'workreferencenumber' : 'CT0224443466',
+  'promotername' : 'JOHN CARRINGTON',
+  'promoteragent' : 'FM CONWAY LTD',
+  'highway-authority' : 'Highway Authority',
+  'usrn' : '08400189',
+  'start-day' : '28',
+  'start-month' : '03',
+  'start-year' : '2017',
+  'end-day' : '30',
+  'end-month' : '03',
+  'end-year' : '2017',
+  'date-flexible-group' : 'Yes',
+  'description-of-work' : 'Digging up the road',
+  'description-of-work-group' : 'Carriageway resurfacing',
+  'contact-details' : '004455500000',
+  'commercially-sensitive-group' : 'No'}
+}
+
 // Route index page
 router.get('/', function (req, res) {
   res.render('index')
@@ -31,20 +50,24 @@ router.post('/alpha/v2-0/create-new-work.html', function (req, res) {
   }
 })
 
+router.get('/alpha/v2-0/work-record', function (req, res) {
+  res.render('alpha/v2-0/work-record.html', data)
+})
+
 router.get('/alpha/v2-0/edit-work-record', function (req, res) {
-  var data = {'data' :{
-      'work-reference-number' : 'CT0224443466',
-      'promoter-name' : 'JOHN CARRINGTON',
-      'promoter-agent' : 'FM CONWAY LTD',
-      'highway-authority' : 'Highway Authority',
-      'usrn' : '08400189',
-      'description-of-work' : 'Digging up the road' }
-    }
   res.render('alpha/v2-0/edit-work-record.html', data)
 })
 
 router.post('/alpha/v2-0/edit-work-record.html', function (req, res) {
-  res.redirect('/alpha/v2-0/edit-success.html')
+  if (req.body.workreferencenumber === '') {
+    res.render('alpha/v2-0/edit-work-record.html', {validationWorkReferenceError: 'Enter the Work reference number', validationError: 'There was a problem'})
+  } else if (req.body.promotername === '') {
+    res.render('alpha/v2-0/edit-work-record.html', {validationPromoterNameError: 'Enter the Promoter name', validationError: 'There was a problem'})
+  } else if (req.body.promoteragent === '') {
+    res.render('alpha/v2-0/edit-work-record.html', {validationPromoterAgentError: 'Enter the Promoter agent', validationError: 'There was a problem'})
+  } else {
+    res.redirect('/alpha/v2-0/edit-success.html')
+  }
 })
 
 router.get('/hello-world', function (req, res) {
