1c81847ef36b85d4800794fcfc0b241a863cd323
Síona Murray <s.murray@kainos.com>
2018-01-24 11:59:04 +0000

    Removing redundant files following UX feedback

--- app/routes.js
+++ app/routes.js
@@ -141,14 +141,6 @@ router.post('/alpha/v3-0/promoter-planner/screen4-check-answers', function (req,
   res.render('alpha/v3-0/promoter-planner/screen4-check-answers.html')
 })
 
-router.post('/alpha/v3-0/work-record-collaboration', function (req, res) {
-  res.render('alpha/v3-0/who-check-answers.html')
-})
-
-router.post('/alpha/v3-0/work-record-check-answers', function (req, res) {
-  res.render('alpha/v3-0/what-check-answers.html')
-})
-
 router.post('/alpha/v3-0/promoter-planner/attach-to-record.html', function (req, res) {
   res.redirect('/alpha/v3-0/promoter-planner/attach-to-record-check.html')
 })
