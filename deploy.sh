9b9ab8d5e1427fe5125a256754c0512fbf954a03
Michal Schott <schott.michal@gmail.com>
2018-02-05 12:34:10 +0100

    Update.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ export REPO=$1
 export NAME=$2
 export TAG=$(git rev-parse HEAD)
 
-gcloud auth activate-service-account continiousintegrationdelivery@lyrical-bolt-194013.iam.gserviceaccount.com --key-file=~/cicd.json
+echo ${GOOGLE_CREDENTIALS} | base64 --decode > ${HOME}/.cicd.json
+gcloud auth activate-service-account continiousintegrationdelivery@lyrical-bolt-194013.iam.gserviceaccount.com --key-file=${HOME}/cicd.json
 docker tag ${REPO}/${NAME}:${TAG} ${REPO}/${NAME}:latest
 gcloud docker -- push ${REPO}/${NAME}:${TAG}
 gcloud docker -- push ${REPO}/${NAME}:latest
