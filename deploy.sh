a109ee6ac8c66a56142de088b8836a644a6ac159
Michal Schott <schott.michal@gmail.com>
2018-02-05 12:38:06 +0100

    Typo.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export NAME=$2
 export TAG=$(git rev-parse HEAD)
 
 echo ${GOOGLE_CREDENTIALS} | base64 --decode > ${HOME}/.cicd.json
-gcloud auth activate-service-account continiousintegrationdelivery@lyrical-bolt-194013.iam.gserviceaccount.com --key-file=${HOME}/cicd.json
+gcloud auth activate-service-account continiousintegrationdelivery@lyrical-bolt-194013.iam.gserviceaccount.com --key-file=${HOME}/.cicd.json
 docker tag ${REPO}/${NAME}:${TAG} ${REPO}/${NAME}:latest
 gcloud docker -- push ${REPO}/${NAME}:${TAG}
 gcloud docker -- push ${REPO}/${NAME}:latest
