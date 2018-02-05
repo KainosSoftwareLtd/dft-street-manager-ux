f4803631bc71af05baab71f39250ee3bb02a148b
Michal Schott <schott.michal@gmail.com>
2018-02-05 12:08:59 +0100

    Setting up GCR.

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ export REPO=$1
 export NAME=$2
 export TAG=$(git rev-parse HEAD)
 
-$(aws ecr get-login --no-include-email --region eu-west-2)
+echo ${GOOGLE_CREDENTIALS} | base64 --decode > ~/cicd.json
+gcloud auth activate-service-account continiousintegrationdelivery@lyrical-bolt-194013.iam.gserviceaccount.com --key-file=~/cicd.json
 docker tag ${REPO}/${NAME}:${TAG} ${REPO}/${NAME}:latest
-docker push ${REPO}/${NAME}:${TAG}
-docker push ${REPO}/${NAME}:latest
+gcloud docker -- push ${REPO}/${NAME}:${TAG}
+gcloud docker -- push ${REPO}/${NAME}:latest
 
 echo "*** Container was build and published with tags:"
 echo "*** latest"
